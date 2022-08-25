b7869531e87b3b74fb67e2de6a0340a0216d6da1
tjupp <travisjupp@gmail.com>
2022-08-25 14:42:55 -0600

    new proj: mystery organism

---
+++ mystery-organism-starter/main.js
@@ -0,0 +1,22 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+
+
+
+
+
+
+
