27423d7c6315ceecc452ef470339895809485164
tjupp <travisjupp@gmail.com>
2022-08-26 19:05:31 -0600

    mutate() method conditional logic (if statement) works when dna bases not equal.
    vars created:
    randNum, randBase, rrb
    (fixed conditional logic errors)
    
    Tried a while loop which mutated dna multiple times, but would be ideal.

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -13,10 +13,56 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// factory function pAequorFactory() that has two parameters:
+
+// The first parameter is a number (no two organisms should have the same number).
+// The second parameter is an array of 15 DNA bases.
+// pAequorFactory() should return an object that contains the properties specimenNum and dna that correspond to the parameters provided.
+
+let num = 0;
+function counter () {
+  return num++;
+};
+
+const pAequorFactory = () => {
+  return specObj = {
+    specimenNum: counter(),
+    dna: mockUpStrand(),
+    // Your team wants you to simulate P. aequor‘s high rate of mutation (change in its DNA).
+    // To simulate a mutation, in pAequorFactory()‘s returned object, add the method .mutate().
+    // .mutate() is responsible for randomly selecting a base in the object’s dna property and changing the current base to a different base. Then .mutate() will return the object’s dna.
+    // For example, if the randomly selected base is the 1st base and it is 'A', the base must be changed to 'T', 'C', or 'G'. But it cannot be 'A' again.
+    mutate() {
+      console.log('==mutate()==');
+      console.log(this.dna);
+      let randNum = Math.floor(Math.random()*this.dna.length);
+      let randBase = this.dna[randNum];
+      let rrb = returnRandBase();
+      // console.log('select random base:',this.dna[Math.floor(Math.random()*this.dna.length)]);
+      
+      if (randBase !== rrb) {
+        console.log('bases not equal','randBase:',randBase);
+        this.dna[randNum] = 'R:'+rrb;
+      } else {
+        console.log('bases equal, try again');
+      }
+      // randBase = 'R:'+returnRandBase();
+      // console.log(this.dna);
+      return this.dna;
+    }
+  }
+};
+
+// console.log(pAequorFactory());
+// console.log(pAequorFactory());
+// console.log(pAequorFactory());
+console.log('pAequorFactory().mutate():\n',pAequorFactory().mutate());
+// console.log(specObj);
+// console.log(JSON.stringify(specObj));
+// console.log('specObj.mutate():\n',specObj.mutate());
+
+// console.log(counter());
+// console.log(counter());
 
 
-
-
-
-
-
+// console.table(mockUpStrand());
