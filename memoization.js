50add4a75d83e9f186f092b75135dc2ed959e2f1
tjupp <travisjupp@gmail.com>
2023-07-26 09:35:14 -0600

    searchLookupTable return condition
    on key not found inhibiting further iterations

--- memoization.js
+++ memoization.js
@@ -1,33 +1,36 @@
 // Memoization: caching computated results, an optimization technique
-const lookupTable = [];
+const lookupTable = []; // the cache
 
 function searchLookupTable(q) {
+    // console.log('searchLookupTable ran');
     for (let ob of lookupTable) {
         for (let key in ob) {
             if (key != q) {
+                console.log(q,'not found');
                 return;
+            } else if (key == q) {
+                console.log(q, 'found');
+                return ob[key];
             }
-            return ob[key];
         }
     }
 }
 
-// searchLookupTable();
-
 function factorial(n) {
     if (n === 0) {
-        console.log('n === 0')
         return 1;
     } else if (searchLookupTable(n)) {
         return searchLookupTable(n);
     } else {
+        console.log('recursive call');
         let x = factorial(n - 1) * n;
         // store x
-        lookupTable.push({[n]: x});
+        lookupTable.push({ [n]: x });
         return x;
     }
 }
 
-factorial(2);
-factorial(3);
-console.log(JSON.stringify(lookupTable))
+factorial(5);
+console.log(JSON.stringify(lookupTable));
+factorial(7);
+console.log(JSON.stringify(lookupTable));
