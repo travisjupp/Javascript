aa15b1336d05eb802edf2fabf2f1e8e8bae38762
tjupp <travisjupp@gmail.com>
2023-08-13 17:23:16 -0600

    add alternate syntax

--- objectMethods.js
+++ objectMethods.js
@@ -2,25 +2,24 @@ let retreatMessage = 'We no longer wish to conquer your planet. It is full of do
 
 // Object methods()
 let alienShip = {
-  retreat() {
-    console.log(retreatMessage);
+  // ES5 method definition syntax
+  ES5retreat: function() {
+    console.log('ES5 ',retreatMessage);
+  },
+  // ES6 shorthand syntax
+  ES6retreat() {
+    console.log('ES6 ',retreatMessage);
   },
   takeOff() {
     console.log('Spim... Borp... Glix... Blastoff!');
-  }
+  },
+  // ES6 alternate syntax
+  ES6alernateRetreat: () => {
+    console.log('ES6 Alt ',retreatMessage.toUpperCase());
+  },
 };
 // Calling methods
-alienShip.retreat();
+alienShip.ES5retreat();
 alienShip.takeOff();
-
-// Global javascript objects
-
-console.log(console);
-console.log(Object.keys(console));
-// console.log(console.Console);
-for (let key in console) {
-  // console.log(key);
-  // console.log(console[key]);
-}
-
-console.log(Math);
+alienShip.ES6retreat();
+alienShip.ES6alernateRetreat();
