5daee8a7e5af4789fe63d72f0ff9b00a44d5b616
tjupp <travisjupp@gmail.com>
2023-10-23 15:35:47 -0600

    new property descriptors js file

---
+++ propertyDescriptors.js
@@ -0,0 +1,36 @@
+// A "Property Descriptor" is a configuration record for an object property
+
+const obj1 = {};
+
+// define a property 
+Object.defineProperty(obj1, "user", {value: "John", writable: true, configurable: true}); // unset keys default to false
+
+// read the property descriptor
+console.log(Object.getOwnPropertyDescriptor(obj1, "user")); // => {value: 'John', writable: true, enumerable: false, configurable: true}
+
+// change an attribute
+Object.defineProperty(obj1, "user", {value: "Jane"});
+console.log(obj1.user) // => Jane
+
+// reconfigure property
+Object.defineProperty(obj1, "user", {writable: false});
+obj1.user = "Ghost";
+console.log(obj1.user) // => Jane (writable is false so assignment has no effect)
+
+// even if writable is false you can still use defineProperty to change the value
+Object.defineProperty(obj1, "user", {value: "God"});
+console.log(obj1.user) // => God
+
+// reconfigure property
+Object.defineProperty(obj1, "user", {writable: true});
+obj1.user = "Mary";
+console.log(obj1.user) // => Mary
+
+
+
+// obj1.user = "Fred";
+
+// console.log(obj1);
+
+// console.log(Object.getOwnPropertyDescriptors(obj1));
+
