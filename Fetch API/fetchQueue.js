c8fe4d06e88ab06b3f45dcd2fbade73803f44d29
tjupp <travisjupp@gmail.com>
2024-06-08 13:17:37 -0600

    Add a function to handle queue

--- Fetch API/fetchQueue.js
+++ Fetch API/fetchQueue.js
@@ -28,7 +28,7 @@ function processQueue() {
                 processQueue(); // Continue processing the queue
             })
             .catch(error => {
-                // console.log('Error: ', error.message);
+                console.log('Error: ', error.message);
                 // Handle fetch error
                 isFetching = false;
                 processQueue(); // Continue processing the queue
@@ -36,11 +36,20 @@ function processQueue() {
     }
 }
 
+function handleQueue(request) {
+    fetchQueue.push(request);
+    processQueue();
+
+}
+handleQueue({ url: 'http://192.168.0.5:8000/user/405_notallowed', delay: 2000 });
+handleQueue({ url: 'http://192.168.0.5:8000/user/Adventurous_Ant7239', delay: 3000 });
+handleQueue({ url: 'http://192.168.0.5:8000/r/MapPorn', delay: 2000 });
+handleQueue({ url: 'https://www.reddit.com/user/lolifax/about.json', delay: 2000 });
 // Usage example
-fetchQueue.push(
-    { url: 'http://192.168.0.5:8000/user/405_notallowed', delay: 5000 },
-    { url: 'http://192.168.0.5:8000/user/Adventurous_Ant7239', delay: 10000 },
-    { url: 'http://192.168.0.5:8000/r/MapPorn', delay: 5000 },
+// fetchQueue.push(
+    // { url: 'http://192.168.0.5:8000/user/405_notallowed', delay: 2000 },
+    // { url: 'http://192.168.0.5:8000/user/Adventurous_Ant7239', delay: 3000 },
+    // { url: 'http://192.168.0.5:8000/r/MapPorn', delay: 2000 },
     // { url: 'https://www.reddit.com/user/lolifax/about.json', delay: 2000 },
     // { url: 'https://www.reddit.com/user/Deep-Neighborhood587/about.json', delay: 1000 },
     // { url: 'https://www.reddit.com/user/RatsWithLongTails/about.json', delay: 1000 },
@@ -49,6 +58,6 @@ fetchQueue.push(
     // { url: 'https://www.reddit.com/user/rememberall/about.json', delay: 1000 },
     // { url: 'https://www.reddit.com/user/SM-68/about.json', delay: 1000 }
     // { url: 'https://www.reddit.com/user/[deleted]/about.json', delay: 15000 }
-);
+// );
 // fetchQueue.push({ url: 'http://192.168.0.5:8000/r/MapPorn', delay: 5000 }); // Add fetch request to the queue
 processQueue(); // Start processing the queue
