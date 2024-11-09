1234303db0e37de7aa94f9eacd2b644f8f16d29e
tjupp <travisjupp@gmail.com>
2024-11-08 17:28:11 -0700

    Create dsa node example

---
+++ dsa-node.js
@@ -0,0 +1,43 @@
+// Node data structures
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	setNextNode(node) {
+		// Set next Node validation
+		if (node === null || node instanceof Node) {
+			this.next = node;
+		} else {
+			throw new Error('node not instance of node 💀');
+		}
+	}
+
+	getNextNode() {
+			return this.next;
+	}
+}
+
+// const firstNode = new Node('instance of Node!');
+// firstNode.setNextNode('not Node instance'); // check validation
+// firstNode.setNextNode(new Node('second Node'));
+
+// create nodes
+const strawberryNode = new Node('Berry Tasty');
+const coconutNode = new Node('Coconuts for Coconut');
+const vanillaNode = new Node('Vanilla');
+
+// set next nodes
+vanillaNode.setNextNode(strawberryNode);
+strawberryNode.setNextNode(coconutNode);
+
+// set current node
+let currentNode = vanillaNode;
+
+while (currentNode) {
+	console.log(currentNode.data);
+  currentNode = currentNode.getNextNode();
+}
+
+module.exports = Node;
