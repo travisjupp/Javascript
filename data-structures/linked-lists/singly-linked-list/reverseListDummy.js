ed9f82816c982fc41e58e99fcb326ccab3212014
tjupp <travisjupp@gmail.com>
2024-11-29 21:17:26 -0700

    Use print node util to view process

--- data-structures/linked-lists/singly-linked-list/reverseListDummy.js
+++ data-structures/linked-lists/singly-linked-list/reverseListDummy.js
@@ -1,63 +1,65 @@
 // Reverse a Singly-Linked List (dummy node)
 
 const util = require('util');
+util.inspect.defaultOptions.depth = null; // show complete objects
 const d = require('../printNode');
-util.inspect.defaultOptions.depth = null; // Set default to unlimited depth
-
 const Node = require('./Node');
 const LinkedList = require('./LinkedList');
 
 const reverseListDummy = (list) => {
 
-	const dummy = new Node('X');
+	const dummy = new Node(0);
 	dummy.next = list.head;
-	// console.log('d', dummy)
 	prev = dummy;
 	curr = list.head;
 
-console.log('-----------^-----------');
+	console.log('-----------^-----------');
 
 	while (curr && curr.next) {
-console.log('DUMM ', d(dummy));
-console.log('CURR ', d(curr));
-
 		let nextTemp = curr.next; // B->C->D->n
+		d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
 
-console.log('\nPOINT', curr.data, '-> ', d(nextTemp.next));
-
+		d('POINT', curr, '->', nextTemp.next, 'hc');
 		curr.next = nextTemp.next; // point  A -> C
+		d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
 
-console.log('\nPOINT', nextTemp.data, '->', prev.next.data);
-
+		d('POINT', nextTemp, '->', prev.next, 'hc');
 		nextTemp.next = prev.next; // point B -> A
+		d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
 
-console.log('\nPOINT', prev.data, '->', d(nextTemp))
-
+		d('POINT', prev, '->', nextTemp, 'hc');
 		prev.next = nextTemp; // point 0 -> B
+		d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
 
-console.log('-----------------------');
+		console.log('-----------------------');
 	}
 
-console.log('DUMM ', d(dummy));
-console.log('CURR ', d(curr));
-console.log('-----------$-----------');
-
-	// const newList = new LinkedList();
-	// newList.head = dummy.next;
-	// return newList;
+	console.log('-----------$-----------');
 
 	list.head = dummy.next;
 	return list;
 }
 
-//     (    start of loop     )    ( swap nodes )    ( setup next iter )
-// i   prev         curr            curr -> prev      prev            curr
-// -----------------------------------------------------------------------------
-// 1: 
-// 2: 
-// 3:
-// 4: 
-
+// -----------^-----------
+// DUMMY 0→A→B→C→D→n CURR A→B→C→D→n
+// POINT A -> C
+// POINT B -> A
+// POINT 0 -> B
+// DUMMY 0→B→A→C→D→n CURR A→C→D→n
+// -----------------------
+// DUMMY 0→B→A→C→D→n CURR A→C→D→n
+// POINT A -> D
+// POINT C -> B
+// POINT 0 -> C
+// DUMMY 0→C→B→A→D→n CURR A→D→n
+// -----------------------
+// DUMMY 0→C→B→A→D→n CURR A→D→n
+// POINT A -> n
+// POINT D -> C
+// POINT 0 -> D
+// DUMMY 0→D→C→B→A→n CURR A→n
+// -----------------------
+// -----------$-----------
 const list = new LinkedList();
 list.addToTail('A');
 list.addToTail('B');
