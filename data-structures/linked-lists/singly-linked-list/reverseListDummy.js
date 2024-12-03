146e625e56ff2e987c7fae653e60e10f4fd06212
tjupp <travisjupp@gmail.com>
2024-12-02 18:27:27 -0700

    Rename `nextTemp` to `temp`

--- data-structures/linked-lists/singly-linked-list/reverseListDummy.js
+++ data-structures/linked-lists/singly-linked-list/reverseListDummy.js
@@ -14,51 +14,44 @@ const reverseListDummy = (list) => {
   prev = dummy;
   curr = list.head;
 
-  console.log('-----------^-----------');
+  d('^');
 
   while (curr && curr.next) {
-    let nextTemp = curr.next; // B->C->D->n
-    // d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
-    d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp);
+    let next = curr.next; // B->C->D->n
+    d('DUMMY', dummy, 'CURR', curr, 'NEXT', next, 'c');
+    // d('DUMMY', dummy, 'CURR', curr, 'NEXT', next);
 
-    // d('POINT', curr, '->', nextTemp.next, 'hc');
-    curr.next = nextTemp.next; // point  A -> C
-    // d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
+    d('POINT', curr, '->', next.next, 'hc');
+    curr.next = next.next; // point  A -> C
+    d('DUMMY', dummy, '  CURR', curr, '  NEXT', next, 'c');
 
-    // d('POINT', nextTemp, '->', prev.next, 'hc');
-    nextTemp.next = prev.next; // point B -> A
-    // d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
+    d('POINT', next, '->', prev.next, 'hc');
+    next.next = prev.next; // point B -> A
+    d('DUMMY', dummy, '  CURR', curr, '  NEXT', next, 'c');
 
-    // d('POINT', prev, '->', nextTemp, 'hc');
-    prev.next = nextTemp; // point 0 -> B
-    // d('DUMMY', dummy, 'CURR', curr, 'NEXTTEMP', nextTemp, 'c');
+    d('POINT', prev, '->', next, 'hc');
+    prev.next = next; // point 0 -> B
+    d('DUMMY', dummy, 'CURR', curr, '  NEXT', next, 'c');
 
-    console.log('-----------------------');
+    d('i');
   }
-
-  console.log('-----------$-----------');
-
+  d('DUMMY', dummy, 'CURR', curr, 'c');
   list.head = dummy.next;
+  d('$');
   return list;
 }
 
 // -----------^-----------
 // DUMMY 0→A→B→C→D→n CURR A→B→C→D→n
-// POINT A -> C
-// POINT B -> A
-// POINT 0 -> B
+// POINT A -> C, POINT B -> A, POINT 0 -> B
 // DUMMY 0→B→A→C→D→n CURR A→C→D→n
 // -----------------------
 // DUMMY 0→B→A→C→D→n CURR A→C→D→n
-// POINT A -> D
-// POINT C -> B
-// POINT 0 -> C
+// POINT A -> D, POINT C -> B, POINT 0 -> C
 // DUMMY 0→C→B→A→D→n CURR A→D→n
 // -----------------------
 // DUMMY 0→C→B→A→D→n CURR A→D→n
-// POINT A -> n
-// POINT D -> C
-// POINT 0 -> D
+// POINT A -> n, POINT D -> C, POINT 0 -> D
 // DUMMY 0→D→C→B→A→n CURR A→n
 // -----------------------
 // -----------$-----------
