37ed7bf461091cf9d601a60272cf24ff285eaf9d
tjupp <travisjupp@gmail.com>
2025-01-21 06:07:05 -0700

    Create recursive fibonacci script

---
+++ l33t/README.md
@@ -0,0 +1,6 @@
+# Leetcode
+
+This folder contains examples and solutions to leet code problems that can be found at [leetcode.com](leetcode.com/problems)
+
+Solutions to problems by other leet-coders in this folder should have credit given in the comments section of the file containing.
+
---
+++ l33t/recursiveFibonacci.js
@@ -0,0 +1,18 @@
+// The algorithms growth rate doubles every time the input data set is added. 
+// This means the time complexity is exponential with an order O(2^n).
+// https://www.freecodecamp.org/news/big-o-cheat-sheet-time-complexity-chart/
+
+// Fib.  0    1    1    2    3    5    8
+//       |    |    |    |    |    |    |
+// Pos. F_0  F_1  F_2  F_3  F_4  F_5  F_6
+
+const recursiveFibonacci = (n) => {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + 
+    recursiveFibonacci(n - 2);
+};
+
+console.log(recursiveFibonacci(4));
+
