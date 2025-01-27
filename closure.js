2ed52a09380002ca975aa69a916be62ec573bce9
tjupp <travisjupp@gmail.com>
2025-01-27 13:44:30 -0700

    Add 3 level example

--- closure.js
+++ closure.js
@@ -24,6 +24,7 @@ inc2(); // => 1 (not 3)
 
 
 // Partial functions
+// Ex 1
 
 function ballType(type) {
   return function(color) {
@@ -31,14 +32,30 @@ function ballType(type) {
   }
 }
 
-let dodge = ballType('dodgeball'); // partially apply ballType
+const dodge = ballType('dodgeball'); // partially apply ballType
 console.log(dodge('red')); // => red dodgeball
 console.log(dodge('black')); // => black dodgeball
-let base = ballType('baseball');
+const base = ballType('baseball');
 console.log(base('white')); // => white baseball
 
+// Ex 2
 
+function object(object) {
+  return function(color) {
+    return function(size) {
+      return size + ' ' + color + ' ' + object;
+    }
+  }
+}
+const color = object('cube');
+let size = color('blue');
+console.log(size('large')); // => large blue cube
+size = color('orange');
+console.log(size('small')); // => small orange cube
+
+// Ex 3
 // https://www.freecodecamp.org/news/how-to-use-partial-application-to-improve-your-javascript-code-5af9ad877833/
+
 const list = (lastJoin, ...items) => {
   const commaSeparated = items.slice(0,-1).join(", ");
   const lastItem = items.pop();
