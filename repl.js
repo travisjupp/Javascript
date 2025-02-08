0fd6b1ec06bc03e033ae584f8ce314df1af62cb3
tjupp <travisjupp@gmail.com>
2025-02-08 16:28:29 -0700

    Update node repl output opts

--- repl.js
+++ repl.js
@@ -1,4 +1,12 @@
 import repl from 'node:repl';
+import util from 'node:util';
 import {style} from './styles.js';
-repl.start('\x1b[38;2;255;0;255m▶ \x1b[0m');
+// repl.start('\x1b[38;2;255;0;255m▶ \x1b[0m');
+repl.start({
+    prompt: '\x1b[38;2;255;0;255m▶ \x1b[0m',
+    writer: (output) => {
+        // Use util.inspect to format output
+        return util.inspect(output, { compact: true, breakLength: Infinity, colors: true });
+    }
+});
 
