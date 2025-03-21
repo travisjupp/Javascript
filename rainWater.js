2c8bf56274e2d503b6d204ca492b419e150790cc
tjupp <travisjupp@gmail.com>
2025-03-20 20:20:57 -0600

    Create Capturing Rain Water problem

---
+++ rainWater.js
@@ -0,0 +1,27 @@
+const collectRainWater = heights => {
+  let leftBound = 0;
+  let rightBound = 0;
+  let leftIdx = 0;
+  let rightIdx = heights.length - 1;
+  let rainWater = 0;
+
+  while (leftIdx < rightIdx) {
+    let l = heights[leftIdx];
+    let r = heights[rightIdx];
+
+    if (l <= r) {
+      if (l > leftBound) leftBound = l;
+      rainWater += leftBound - l;
+      leftIdx++;
+    } else {
+      if (r > rightBound) rightBound = r;
+      rainWater += rightBound - r;
+      rightIdx--;
+    }
+  }
+  console.log('rainWater: ', rainWater);
+
+}
+
+export default collectRainWater;
+
