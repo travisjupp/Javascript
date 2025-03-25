321eb0a7013e4830f397a1268858ffa1226ca05f
tjupp <travisjupp@gmail.com>
2025-03-25 16:58:05 -0600

    Comment explicit sort with comparator func

--- explicitSortWithComparator.js
+++ explicitSortWithComparator.js
@@ -1,28 +1,31 @@
 const explicitSortWithComparator = (inputArray, order) => {
 
-  const explicitComparator = (a, b) => {
+  const explicitComparator = (A, B) => {
     let indexA = order.length;
     let indexB = order.length;
 
-    if (order.includes(a)) {
-      indexA = order.indexOf(a);
+    if (order.includes(A)) {
+      indexA = order.indexOf(A);
     }
-    if (order.includes(b)) {
-      indexB = order.indexOf(b);
+    if (order.includes(B)) {
+      indexB = order.indexOf(B);
     }
 
+    /******** log sort-order ********/
     function test() {
-      return indexA - indexB >0 ? b+a
-        : indexA - indexB === 0 ? b+a
-        : a+b;
+      return indexA - indexB > 0 ? B+A
+        : indexA - indexB === 0 ? B+A
+        : A+B;
     }
-    console.log(`A:${a} indexA:${indexA} ---\
-    B:${b} indexB:${indexB} ---\
+    console.log(`A:${A} indexA:${indexA} ---\
+    B:${B} indexB:${indexB} ---\
     indexA - indexB = ${indexA - indexB} ---\
-    ${indexA - indexB > 0 ? 'b before a' : indexA - indexB === 0 ? 'no swap' : 'a before b'}`, `input: ${test()}`);
+    ${indexA - indexB > 0 ? 'B before A' : indexA - indexB === 0 
+        ? 'no swap' : 'A before B'}`, `input: ${test()}`);
+    /******** /log sort-order ********/
+
     return indexA - indexB; 
   }
-
   return inputArray.sort(explicitComparator).slice();
 }
 
