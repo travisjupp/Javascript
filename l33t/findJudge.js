bbff32341f18d2128cb932f0a341e4e2eab25f1f
tjupp <travisjupp@gmail.com>
2025-04-03 06:46:21 -0600

    Wordwrap description

--- l33t/findJudge.js
+++ l33t/findJudge.js
@@ -6,12 +6,15 @@
 //
 // If the town judge exists, then:
 //
-// The town judge trusts nobody.
-// Everybody (except for the town judge) trusts the town judge.
-// There is exactly one person that satisfies properties 1 and 2.
-// You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust relationship does not exist.
+// The town judge trusts nobody.  Everybody (except for the town judge) trusts
+// the town judge.  There is exactly one person that satisfies properties 1 and
+// 2.  You are given an array trust where trust[i] = [ai, bi] representing that
+// the person labeled ai trusts the person labeled bi. If a trust relationship
+// does not exist in trust array, then such a trust relationship does not
+// exist.
 //
-// Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+// Return the label of the town judge if the town judge exists and can be
+// identified, or return -1 otherwise.
 
 var findJudge = function(N, trust) {
     const inDegree = new Array(N + 1).fill(0);
