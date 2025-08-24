dd5b3c5637aaf434067191adc9e73c9a32a1b678
tjupp <travisjupp@gmail.com>
2025-08-24 08:34:11 -0600

    Update ANSI styles `styles.js`

--- styles.js
+++ styles.js
@@ -1,24 +1,91 @@
 // ANSI styles
-// usage: console.log(style.color(255,0,255),'▷',style.reset,style.color(39),expect.getState().currentTestName,style.hr.double,style.reset); });
+// usage: import and run `style.test()` for samples, see styles.test for console log examples
+//
+// someFile.js
+// import {style} from 'styles.js';
+// style.test();
+
 const style = {
   reset: '\x1b[0m',
   bold: '\x1b[1m',
   italic: '\x1b[3m',
+  bolditalic: '\x1b[3m\x1b[1m',
   underline: '\x1b[4m',
   dblUnderline: '\x1b[21m',
   dim: '\x1b[2m',
   invert: '\x1b[7m',
   black: '\x1b[30m',
+  blackbg: '\x1b[40m',
   red: '\x1b[31m',
+  redbg: '\x1b[41m',
   orange: '\x1b[38;5;208m',
   green: '\x1b[32m',
+  greenbg: '\x1b[42m',
   yellow: '\x1b[33m',
+  yellowbg: '\x1b[43m',
   blue: '\x1b[34m',
+  bluebg: '\x1b[44m',
+  purple: '\x1b[35m',
+  purplebg: '\x1b[45m',
   magenta: '\x1b[35m',
   cyan: '\x1b[36m',
+  cyanbg: '\x1b[46m',
   white: '\x1b[37m',
+  whitebg: '\x1b[47m',
   primaryFont: '\x1b[10m',
   strike: '\x1b[9m',
+  chain: (...styles) => {
+    let chainedStyles = '';
+    for (const styleName of styles) {
+      chainedStyles += style[styleName];
+    }
+    return chainedStyles;
+  },
+  wrap: (...styles) => {
+    let chainedStyles = '';
+    for (const styleName of styles) {
+      if (styles.indexOf(styleName) === styles.length -1) {
+        chainedStyles += styleName;
+      } else {
+        chainedStyles += style[styleName];
+      }
+    }
+    chainedStyles += style.reset;
+    return chainedStyles;
+  },
+  list: (...listItems) => {
+    let list = '';
+    for (const item of listItems) {
+      list += '\n ☞  '+item;
+    }
+    return list;
+  },
+  todo: (...listItems) => {
+    let list = '';
+    for (const item of listItems) {
+      if (item[0] === '+') {
+      list += '\n - [ ] '+item.slice(1);
+      }
+      if (item[0] === '-') {
+      list += '\n - [x] '+item.slice(1);
+      }
+      if (item[0] === '~') {
+      list += '\n - [~] '+style.strike+item.slice(1)+style.reset;
+      }
+      if (item[0] === '!') {
+        if (item[1] === '+') {
+          list += '\n - [ ] '+style.bold+item.slice(2)+style.reset;
+        }
+        if (item[1] === '-') {
+          list += '\n - [x] '+style.bold+item.slice(2)+style.reset;
+        }
+        if (item[1] !== '+' && item[1] !== '-') {
+          list += '\n - [ ] '+style.bold+item.slice(1)+style.reset;
+        }
+      }
+    }
+    return list;
+  },
   hr: {
     double: `${'═'.repeat(process.env.COLUMNS - 1 || 80)}`,
     single: `${'─'.repeat(process.env.COLUMNS - 1 || 80)}`
@@ -29,7 +96,66 @@ const style = {
   bgColor(r,g,b) {
     return g === undefined ? `\x1b[48;5;${r}m` : `\x1b[48;2;${r};${g};${b}m`;
   },
+  test: () => {
+    console.log(
+      style.color(255,0,255),'Styled with Custom RGB Color',style.reset,
+      style.color(39),' Some Styled Text',style.reset,
+      style.bgColor(18),style.orange,' Some Background Styled Text',style.reset,
+      style.bgColor(0,255,0),style.black,style.bold,' Some Background Styled Text',style.reset,
+      style.hr.double,style.reset,
+      style.wrap('italic', 'Make me ITALIC (reset included for `style.wrap()`)'),
+      style.wrap('black', 'redbg', 'bold', 'strike', 'Make me RED BACKGROUND STRIKED'),
+      style.chain('red', 'bold'), 'Make me RED BOLD', style.reset,
+      style.dim, 'Make me DIM', style.reset,
+      style.list('Make me a LIST ITEM','Make me a LIST ITEM'), style.reset,
+      style.list(`Make me a ${style.italic}INLINE STYLED${style.reset} LIST ITEM`,`Make me a LIST ITEM`), style.reset,
+      style.todo('+Make me a TODO LIST ITEM','-Make me a DONE LIST ITEM','~Make me a STRIKED LIST ITEM','!Make me a IMPORTANT TODO LIST ITEM', '!+Make me a IMPORTANT TODO LIST ITEM', '!-Make me a IMPORTANT DONE LIST ITEM'), style.reset,
+    );
+  },
+  help: () => {
+    console.log(`
+@tjupp ANSI styles
+usage: import help and test for demo
+*************************************************
+// someFileIwantToStyleConsoleLogsIn.js
+
+import {style} from 'styles.js';
+style.test();
+style.help();
+console.log(style.bold, 'Bold ME!', style.reset);
+*************************************************
+Wrap in console logs 
+E.g., console.log(style.bold, 'Bold ME!', style.reset);
+
+SAMPLES (run \`styles.test()\` to view styled in console)
+
+  style.color(255,0,255),'Styled with Custom RGB Color',style.reset,
+  style.color(39),' Some Styled Text',style.reset,
+  style.bgColor(18),style.orange,' Some Background Styled Text',style.reset,
+  style.bgColor(0,255,0),style.black,style.bold,' Some Background Styled Text',style.reset,
+  style.hr.double,style.reset,
+  style.wrap('italic', 'Make me ITALIC (reset included for \`style.wrap()\`)'),
+  style.wrap('black', 'redbg', 'bold', 'strike', 'Make me RED BACKGROUND STRIKED'),
+  style.chain('red', 'bold'), 'Make me RED BOLD', style.reset,
+  style.dim, 'Make me DIM', style.reset,
+
+  style.list( 'Make me a LIST ITEM', 'Make me a LIST ITEM'), style.reset,
+
+  style.list(
+    \`Make me a \${style.italic}INLINE STYLED\${style.reset} LIST ITEM\`,
+    \`Make me a LIST ITEM\`), style.reset,
+
+  style.todo(
+    '+Make me a TODO LIST ITEM',
+    '-Make me a DONE LIST ITEM',
+    '~Make me a STRIKED LIST ITEM',
+    '!Make me a IMPORTANT TODO LIST ITEM', 
+    '!+Make me a IMPORTANT TODO LIST ITEM', 
+    '!-Make me a IMPORTANT DONE LIST ITEM'), style.reset,
+    `);
+  }
 }
 
-export {style}
+export { style }
+// @tjupp
 
