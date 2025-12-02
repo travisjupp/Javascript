79330b3256cc70e3f417a322690b3ed493057762
tjupp <travisjupp@gmail.com>
2025-12-02 05:00:24 -0700

    Wrap param in parens for Conjure
    - Conjure evaluation breaks when single arrow function params not
      wrapped in parens. Wrapping in parens, fix config later...

--- arrayConvert.js
+++ arrayConvert.js
@@ -1,14 +1,13 @@
 // Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '.
 
-const convertToBaby = arr => {
-    let babyArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        babyArray.push(`baby ${arr[i]}`);
-    }
-    return babyArray;
+const convertToBaby = (arr) => {
+  const babyArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    babyArray.push(`baby ${arr[i]}`);
+  }
+  return babyArray;
 }
 
-// When you're ready to test your code, uncomment the below and run:
 const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
 
 console.log(convertToBaby(animals))
