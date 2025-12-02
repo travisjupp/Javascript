7094a8d0866883dbee923a7ca0baa01efec9e108
tjupp <travisjupp@gmail.com>
2025-12-02 05:08:55 -0700

    Create Promise Sequence Piping js file
    - MDN example to play with demonstrating using Array.prototype.reduce on
      a sequence of Promises

---
+++ promSeqPipe.js
@@ -0,0 +1,20 @@
+/* Promise sequencing is essentially function piping demonstrated in the previous section, except done asynchronously.
+*/
+
+// Compare this with pipe: fn(acc) is changed to acc.then(fn),
+// and initialValue is ensured to be a promise
+const asyncPipe =
+  (...functions) =>
+  (initialValue) =>
+    functions.reduce((acc, fn) => acc.then(fn), Promise.resolve(initialValue));
+
+// Building blocks to use for composition
+const p1 = async (a) => a * 5;
+const p2 = async (a) => a * 2;
+// The composed functions can also return non-promises, because the values are
+// all eventually wrapped in promises
+const f3 = (a) => a * 3;
+const p4 = async (a) => a * 4;
+
+asyncPipe(p1, p2, f3, p4)(10).then(console.log); // 1200
+
