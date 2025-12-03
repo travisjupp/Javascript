74257c78cef1fe89de790e7e89e5d6702885e08e
tjupp <travisjupp@gmail.com>
2025-12-03 09:36:53 -0700

    Checking style rendering

--- map.test.js
+++ map.test.js
@@ -79,10 +79,10 @@ describe('Map.groupBy()', () => {
 
     console.log(style.h1('# The Map.groupBy() static method groups the elements of a given iterable using the values returned by a provided callback function. The final returned Map uses the unique values from the test function as keys, which can be used to get the array of elements in each group\n'));
 
-    console.log(style.h2('## Given an iterable (inventory):\n\n'),
+    console.log(style.h2(`## Given an iterable (inventory):\n\n`),
       inventory, '\n');
 
-    console.log(style.h2('## ..And grouping values (restock, sufficient):\n\n'),  restock, '\n', sufficient, '\n');
+    console.log(style.h2(`## ..And grouping values (restock, sufficient):\n\n`),  restock, `\n`, sufficient, `\n`);
 
     console.log(style.h2(
       `## ..Using \`Map.groupBy()\` with a cb that conditionally returns
